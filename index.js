50f660c6444ff1efbc5dafed2ce9db09339b31f6
Josh-Erik1 <jseriki21@gmail.com>
2023-05-21 19:24:14 +0100

    FAQ accordion activated

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const bars = document.querySelectorAll(".bars");
 const navMenu = document.querySelector(".nav__list");
 const navLink = document.querySelectorAll(".nav__link");
 
+const faqToggle = document.querySelectorAll(".faq__toggle");
+
 hamburger.addEventListener("click", () => {
-  console.log("clicked");
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 });
@@ -13,5 +14,15 @@ navLink.forEach((link) => {
   link.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
+  });
+});
+
+faqToggle.forEach((toggle) => {
+  toggle.addEventListener("click", () => {
+    toggle.classList.toggle("open");
+
+    const faqAnswer = toggle.parentElement.nextElementSibling;
+
+    faqAnswer.classList.toggle("hide");
   });
 });
